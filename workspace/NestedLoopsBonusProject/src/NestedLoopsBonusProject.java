6950682cb71d4e4ced5bda80523e40ddace78a60
Alex Friedberg <blueal22@hotmail.com>
2015-10-05 00:35:26 -0700

    Finished Nested Loops Bonus
    Only took me half an hour to re-write the whole thing

--- workspace/NestedLoopsBonusProject/src/NestedLoopsBonusProject.java
+++ workspace/NestedLoopsBonusProject/src/NestedLoopsBonusProject.java
@@ -1,54 +1,61 @@
-/* Code for Exercise 2.23
- * 
- * Designed to generate Dollar Figure Shape
- * 
- * SIZE = 3
- * $$$******$$$ 023320
- * **$$****$$** 222222
- * ****$**$****41114
- * 
- * SIZE = 5
- * $$$$$**********$$$$$ 0555550
- * **$$$$********$$$$** 24442
- * ****$$$******$$$**** 43334
- * ******$$****$$******
- * ********$**$********
- * 
- */
-
-
+//I could have decomped this project but it wasn't a requirement so I didn't
 
 public class NestedLoopsBonusProject {
 	//Size Variable
     public static final int TOTAL_STEPS = 5;
-    public static final int STEP_WIDTH = 5; 
+    public static final int STEP_WIDTH = 6; 
 
 	public static void spaceItOut(int i){
 		for(int j = i * STEP_WIDTH; j >= 0; j--){
 			System.out.print(" ");
 		}
 	}
-	
+
 	public static void main(String[] args){
-		for(int i = TOTAL_STEPS; i >= 0; i--){
-			for(int j = (i * STEP_WIDTH) - 1; j >= 0; j--){
+		for(int step = TOTAL_STEPS - 1; step >= 0; step--){
+			
+			for(int row = (step * STEP_WIDTH) - 1; row >= 0; row--){
 				System.out.print(" ");
 			}
-			
-			for(int j = 1; j <= STEP_WIDTH; j++){
+			System.out.print(" o  ");
+			for(int stars = STEP_WIDTH; stars >= 1; stars--){
 				System.out.print("*");
 			}
-			
-			//Back of the stair star
-			for(int j = (TOTAL_STEPS * STEP_WIDTH) - (i * STEP_WIDTH); j >= 0; j--){
+			System.out.print("*");
+			for(int j = ((TOTAL_STEPS - step) * (STEP_WIDTH)) - STEP_WIDTH; j >= 1; j--){
 				System.out.print(" ");
 			}
 			System.out.println("*");
+			
+			
+			
+			for(int row = (step * STEP_WIDTH) - 1; row >= 0; row--){
+				System.out.print(" ");
+			}
+			System.out.print("/|\\ *");
+			for(int j = ((TOTAL_STEPS - step) * (STEP_WIDTH)); j >= 1; j--){
+				System.out.print(" ");
+			}
+			System.out.println("*");
+			
+			
+			
+			for(int row = (step * STEP_WIDTH) - 1; row >= 0; row--){
+				System.out.print(" ");
+			}
+			System.out.print("/ \\ *");
+			for(int j = ((TOTAL_STEPS - step) * (STEP_WIDTH)); j >= 1; j--){
+				System.out.print(" ");
+			}
+			System.out.println("*");
+
 		}
 		
-		//Bottom
-		for(int i = 1; i <= (TOTAL_STEPS * STEP_WIDTH); i++){
+		//MAGIC NUMBERS BELOW
+		//
+		for(int i = (STEP_WIDTH + 1) * TOTAL_STEPS; i >= 0; i--){
 			System.out.print("*");
 		}
+		
 	}
 }
