d92ad954705d0947faa36a1574c109aae1b85074
Alex Friedberg <s-afriedberg@486E283464.lwsd.k12wa.org>
2015-10-12 09:15:44 -0700

    edited tester

--- workspace/Tester/src/Confusing.java
+++ workspace/Tester/src/Confusing.java
@@ -1,18 +1,24 @@
 public class Confusing {
-    public static void main(String[] args) {
-        String one = "two";
-        String two = "three";
-        String three = "1";
-        int number = 20;
-
-        sentence(one, two, 3);
-        sentence(two, three, 14);
-        sentence(three, three, number + 1);
-        sentence(three, two, 1);
-        sentence("eight", three, number / 2);
-    }
-
-    public static void sentence(String three, String one, int number) {
-        System.out.println(one + " times " + three + " = " + (number * 2));
+	public static void main(String[] args) {
+	//      index 0123456789012345
+		String str1 = "Q.E.D.";
+		String str2 = "Arcturan Megadonkey";
+		String str3 = "Sirius Cybernetics Corporation";
+	
+		System.out.println(str1.length());	
+		System.out.println(str2.length());	
+		System.out.println(str1.toLowerCase());	
+		System.out.println(str2.toUpperCase());	
+		System.out.println(str1.substring(2, 4)	);
+		System.out.println(str2.substring(10, 14)	);
+		System.out.println(str1.indexOf("D")	);
+		System.out.println(str1.indexOf(".")	);
+		System.out.println(str2.indexOf("donkey"));	
+		System.out.println(str3.indexOf("X")	);
+		System.out.println(str2 + str3.charAt(17)	);
+		System.out.println(str3.substring(9, str3.indexOf("e"))	);
+		System.out.println(str3.substring(7, 12)	);
+		System.out.println(str2.toLowerCase().substring(9, 13) + str3.substring(18, str3.length() - 7));
+	
     }
 }
