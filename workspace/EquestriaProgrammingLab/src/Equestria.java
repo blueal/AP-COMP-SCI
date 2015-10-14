beab178d3ecfd637488ce460c52258f7845227bd
Alex Friedberg <s-afriedberg@486E283464.lwsd.k12wa.org>
2015-10-14 11:47:55 -0700

    Finish distance Equation
    Part of Equestria Lab

--- workspace/EquestriaProgrammingLab/src/Equestria.java
+++ workspace/EquestriaProgrammingLab/src/Equestria.java
@@ -1,4 +1,8 @@
-
 public class Equestria {
-
+	public static void main(String[] args) {
+		System.out.println(distance(0, 26, 0 , -1));
+	}
+	public static double distance(int x1, int x2, int y1, int y2){
+		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));		
+	}
 }
