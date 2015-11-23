34b3ba4ae51e8dd466901bbd0133626b8e4239fa
Alex Friedberg <blueal22@hotmail.com>
2015-11-23 09:18:03 -0800

    Worked on Data Transformation

--- workspace/DataTransformation/src/DataTransformation.java
+++ workspace/DataTransformation/src/DataTransformation.java
@@ -1,6 +1,17 @@
 
 public class DataTransformation {
 	public static void main(String[] args) {
-		
+		String[] arr = {"test", "TESTESTE1", "TESTESTE2"};
+		System.out.println(maxWordLength(arr));
+	}
+	
+	public static String maxWordLength(String[] list){
+		String longestWord = "";
+		for (String listItem : list) {
+		    if(listItem.length() > longestWord.length()){
+		    	longestWord = listItem;
+		    }
+		}
+		return longestWord;
 	}
 }
