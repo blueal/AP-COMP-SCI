9963006844c04bc331682714b62ce61d918a5cfe
Alex Friedberg <blueal22@hotmail.com>
2015-11-25 08:02:05 -0800

    Started excercise 2 for data transformation

--- workspace/DataTransformation/src/DataTransformation.java
+++ workspace/DataTransformation/src/DataTransformation.java
@@ -2,7 +2,31 @@
 public class DataTransformation {
 	public static void main(String[] args) {
 		String[] arr = {"test", "TESTESTE1", "TESTESTE2"};
-		System.out.println(maxWordLength(arr));
+		String test = "Star Wars: Episode VII The Force Awakens";
+		System.out.println(splitString(test));
+	}
+	
+	public static int splitString(String input){
+		
+		//Well start with counting the words in the string
+		int wordCount = 1;
+		for(int i = 0; i < input.length() - 1; i++){
+			if(input.substring(i, i + 1).equals(" ")){
+				wordCount++;
+			}
+		}
+		
+		//Initialize Array
+		String[] output = new String[wordCount];
+	
+		//Start saving words into array slots
+		for(int i = 0; i < input.length() - 1; i++){
+			if(input.substring(i, i + 1).equals(" ")){
+				
+			}
+		}
+		
+		return wordCount;
 	}
 	
 	public static String maxWordLength(String[] list){
