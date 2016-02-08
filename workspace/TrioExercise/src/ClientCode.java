f9619db3b28f113961bebc7e64a48269affb9086
Alex Friedberg <blueal22@hotmail.com>
2016-02-08 08:34:21 -0800

    CientCode for Trio Project

--- workspace/TrioExercise/src/ClientCode.java
+++ workspace/TrioExercise/src/ClientCode.java
@@ -4,8 +4,9 @@ public class ClientCode {
 	Salad salad1 = new Salad("Spinach Salad", 1.25);
 	Drink drink1 = new Drink("Orange Soda", 1.00);
 	
-	System.out.println();
+	
+	
 	Trio trio1 = new Trio(sandwich1, salad1, drink1);
 	
-	System.out.println(trio1);
+
 }
