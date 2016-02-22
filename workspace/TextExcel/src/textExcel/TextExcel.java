dea7ce87bd865418038fb9c8476b89a14194517f
Alex Friedberg <blueal22@hotmail.com>
2016-02-22 07:57:43 -0800

    Imported TextExcel

---
+++ workspace/TextExcel/src/textExcel/TextExcel.java
@@ -0,0 +1,14 @@
+package textExcel;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.Scanner;
+import textExcel.TestsALL.Helper;
+
+// Update this file with your own code.
+public class TextExcel
+{
+	public static void main(String[] args)
+	{
+	}
+}
