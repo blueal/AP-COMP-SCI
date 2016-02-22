39c982bf513bd06de6878e986f711b598644325d
Alex Friedberg <blueal22@hotmail.com>
2016-02-22 08:13:23 -0800

    Implementing classes

---
+++ workspace/TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -0,0 +1,17 @@
+package textExcel;
+
+public class SpreadsheetLocation implements Location {
+
+	@Override
+	public int getRow() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCol() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
