d71fe46b4ec0148e9aeb065ea2ac0538e3a10cc2
Alex Friedberg <blueal22@hotmail.com>
2016-02-24 08:16:23 -0800

    Started abstract method practice

---
+++ workspace/PetAbstractClassExcercise/src/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog extends Pet {
+
+	public Dog(String name) {
+		super(name);
+	}
+
+	@Override
+	public String speak() {
+		return "bark";
+	}
+
+}
---
+++ workspace/PetAbstractClassExcercise/src/Kennel.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Kennel
+{
+	private List<Pet> petList;
+
+	/** @param pet to be added to list. Assume not null.
+	 * Postcondition: pet is added to petList
+	 */
+	public void addPet(Pet pet)
+	{
+		this.petList.add(pet);
+	}
+
+	/** Postcondtion: for each Pet in the kennel, its name
+	 * followed by the result of a call to its
+	 * speak method is printed to the console,
+	 * one line per Pet
+	 */
+	public void allSpeak()
+	{
+		for(int i = 0; i < this.petList.size(); i++){
+			System.out.println(this.petList.get(i).speak());
+		}
+	}
+	
+	
+} 
